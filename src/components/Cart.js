30ccd2f459a5ac82bf435c7f8f9890d8caf4765b
sebastian medina <sebastianlopez_21@hotmail.com>
2022-12-22 11:50:49 -0500

    Adds a table to display items on cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,22 +2,39 @@ import React from "react";
 
 function Cart({cartItems}){
 
-    const addedToCart= cartItems.map(item => (
-        <div key={item.id}className="onCart">
-            <img src={item.image}/>
-            <p>{item.name}</p><p>{item.price}</p>
-            <button>Remove From Cart</button>
-        </div>
+
+   const addedToCart = cartItems.map(item => (
+        <tr>
+            <td>
+                <img src={item.image}/>
+            </td>
+            <td>
+                <p>{item.name}</p>
+            </td>
+            <td>
+                <p>{item.price}</p>
+            </td>
+            <td>
+                <button>Remove From Cart</button>
+            </td>
+        </tr>
     ))
+
     
     console.log(addedToCart);
     return(
         <div className="cart-container">
             <h1>MY CART: </h1>
             <hr/>
-            <ul>
-                {addedToCart}
-            </ul>
+            <table className="onCart">
+                <tr>
+                    <th>ITEM</th>
+                    <th>DESCRIPTION</th>
+                    <th>PRICE</th>
+                </tr>
+                {addedToCart}      
+            </table>
+            <hr/>
         </div>
     )
 
